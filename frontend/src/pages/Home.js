c3776c9f4c98a1f101ddc73cb098f5742cd71e47
Zayan Sheikh <dbzayan@gmail.com>
2024-05-17 01:25:43 -0700

    Fixed file structure

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+import { Tilt } from 'react-tilt';
+
+const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:4000/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div class="workouts">
+        {workouts && workouts.map(workout => (
+      <Tilt options={{max: 10, transition: true, scale: 1.03, easing: "cubic-bezier(0.175, 0.885, 0.32, 1.275)", glare:true}}>
+          <WorkoutDetails workout={workout} key={workout._id} />
+      </Tilt>
+        ))}
+      </div>
+      <WorkoutForm />
+      
+
+    </div>
+  )
+}
+
+export default Home
