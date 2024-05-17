49d206188026244b9dadca0cafd9b1b77575d9e6
Zayan Sheikh <dbzayan@gmail.com>
2024-05-17 02:39:56 -0700

    chore: Update API endpoint URLs to use production server

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -7,7 +7,7 @@ const WorkoutDetails = ({workout}) => {
     const { dispatch } = useWorkoutsContext()
 
     const handleClick = async () => {
-        const response = await fetch('http://localhost:4000/api/workouts/' + workout._id, {
+        const response = await fetch('https://workout-buddy-topaz.vercel.app/api/workouts/' + workout._id, {
             method: 'DELETE'
         })
         const json = await response.json()
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('http://localhost:4000/api/workouts')
+      const response = await fetch('https://workout-buddy-topaz.vercel.app/api/workouts')
       const json = await response.json()
 
       if (response.ok) {
